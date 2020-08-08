b40c7c3b7869638b2d64861e2d9e0a8caab8a64b
Hamza Hayd <43278171+Hamza1001101@users.noreply.github.com>
2020-08-08 17:42:16 +0200

    added custom forminput and custom-button components and styled them

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/HomePage';
 import { Switch, Route } from 'react-router-dom';
 import Shop from './pages/shop/Shop';
 import Header from './components/header/Header';
+import SingUpAndSingIn from './pages/sing-in-and -sing-up/SingUpAndSingIn';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={Shop} />
+        <Route path='/Signin' component={SingUpAndSingIn} />
       </Switch>
     </div>
   );
---
+++ src/pages/sing-in-and -sing-up/SingUpAndSingIn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './sign-in-and-sign-up.scss';
+import SignIn from '../../components/sign-in/SignIn';
+
+const SingUpAndSingIn = () => {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SingUpAndSingIn;
